a89cd90966a9f04f1f31a12b0ac1c23514ee35e5
Rhys Packer <rhys.pacs@yahoo.com>
2016-12-10 03:18:36 +0000

    Added fullscreen video functionality
    With .hero as container of video

--- index.php
+++ index.php
@@ -12,7 +12,22 @@ include('includes/header.php');
 
 ?>
 
+<div class="hero">
+	
+	<video autoplay loop poster="video/assets/bgimage.JPG">
+		<source src="video/assets/bgvid.ogv" type="video/ogv">
+		<source src="video/assets/bgvid.webm" type="video/webm">
+		<source src="video/assets/bgvid.mp4" type="video/mp4">
+	</video>
 
+</div>
+
+<p>ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
      
 
 
