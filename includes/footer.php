e0d3e383348f605512622b24dda7c1fc60f8e7e8
Rhys Packer <rhys.pacs@yahoo.com>
2016-12-10 10:20:35 +0000

    Footer added and styled
    added copyright info

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,24 @@
 
 
 
-<footer>
+<footer class="flex-container">
+
+	<div class="flex-item">
+
+		<h2>OTHER LINKS</h2>
+
+		<ul>
+			<li><a href="terms-conditions.php">Terms &amp; Conditions</a></li>
+			<li><a href="terms-conditions.php">Join the Label</a></li>
+			<li><a href="terms-conditions.php">Contact Us</a></li>
+		</ul>
+	</div>
+
+	<div class="flex-item">
+
+	</div>
+
+	<p id="copyright">&copy; Copyright New Street Records 2017</p>
 
 </footer>
 
