97bb6cdf4968077b6e44cf7bc7a7b243ee92263d
Rhys Packer <rhys.pacs@yahoo.com>
2017-01-25 23:56:32 +0000

    Footer styled and relevant content added. Typography updated now.

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,44 @@
-
-
-
-<footer class="flex-container">
+<footer class="flex-container" role="contentinfo">
 
 	<div class="flex-item">
 
-		<h2>OTHER LINKS</h2>
+		<h2>Other Links</h2>
 
 		<ul>
 			<li><a href="terms-conditions.php">Terms &amp; Conditions</a></li>
 			<li><a href="terms-conditions.php">Join the Label</a></li>
 			<li><a href="terms-conditions.php">Contact Us</a></li>
 		</ul>
-	</div>
-
-	<div class="flex-item">
 
 	</div>
 
-	<p id="copyright">&copy; Copyright New Street Records 2017</p>
+	<div class="flex-item social">
+
+		<h2>Follow Us</h2>
+
+		<a href="#" title="">
+
+			<img src="img/svg/twitter.svg" alt="Kiwi standing on oval">
+
+		</a>
+
+		<a href="#" title="">
+
+			<img src="img/svg/facebook.svg" alt="Kiwi standing on oval">
+
+		</a>
+
+	</div>
+
+	<p id="copyright">&copy; Copyright New Street Records <?php echo date("Y"); ?></p>
 
 </footer>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="js/nav.js"></script>
 
-</body> 
-	
-</html> 
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+</body>
+
+</html>
